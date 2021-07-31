56d038860f72b8b6b9ee0bbf7dd2e1e63fa90480
george <gigwegbe@gmail.com>
2021-07-31 04:38:20 +0100

    Fetching data from a remote server

--- test-app/src/AppContent.js
+++ test-app/src/AppContent.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 
 export default class AppContent extends Component {
 
+    fetchList = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => response.json())
+            .then(json => {
+                console.log(json);
+
+                let posts = document.getElementById("post-list");
+
+                json.forEach(function(obj){
+                    let li = document.createElement("li"); 
+                    li.appendChild(document.createTextNode(obj.title)); 
+                    posts.appendChild(li);
+
+                })
+            })
+    }
+
     render(){
         return (
             <p>This is a content
                 <br/>
-                <button class="btn btn-primary" href='#'>My Button</button>
+                <button onClick={this.fetchList} class="btn btn-primary" href='#'>Fetch Data</button>
+
+                <hr />
+
+                <ul id="post-list"></ul>
             </p>
         )
     }
