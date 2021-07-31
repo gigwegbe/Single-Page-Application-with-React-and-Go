0aa1e17e321e9e186bb46795be09a13857a45118
george <gigwegbe@gmail.com>
2021-07-31 03:31:49 +0100

    Adding AppFooter component

--- test-app/src/index.js
+++ test-app/src/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'; 
 import ReactDOM from 'react-dom';
+import AppFooter from './AppFooter';
 
 class App extends Component {
   render(){
     return (
       <div>
         <h1>Hello World</h1>
+        <AppFooter></AppFooter>
       </div>
     );
   }
