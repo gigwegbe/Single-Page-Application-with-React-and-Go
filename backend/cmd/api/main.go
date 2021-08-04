fa2d84e555ae65d89e8df50a017cd0347aaa3e61
george <gigwegbe@gmail.com>
2021-08-04 05:37:38 +0100

    Adding model

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,31 +1,30 @@
-package main 
+package main
 
 import (
-	"time"
-	"fmt"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
-
 
 const version = "1.2.0"
 
 type config struct {
-	port int 
-	env string 
+	port int
+	env  string
 }
 
 type AppStatus struct {
-	Status  string `json:"status"`
-	Environment  string `json:"environment"`
-	Version  string `json:"version"`
+	Status      string `json:"status"`
+	Environment string `json:"environment"`
+	Version     string `json:"version"`
 }
 
 type application struct {
-	config config 
-	logger *log.Logger 
+	config config
+	logger *log.Logger
 }
 
 func main() {
@@ -35,18 +34,18 @@ func main() {
 	flag.StringVar(&cfg.env, "env", "development", "Application environment(development | production)")
 	flag.Parse()
 
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime )
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
-	app := &application {
-		config: cfg, 
-		logger: logger, 
+	app := &application{
+		config: cfg,
+		logger: logger,
 	}
 
 	srv := &http.Server{
-		Addr: fmt.Sprintf(":%d", cfg.port),
-		Handler: app.routes(),
-		IdleTimeout: time.Minute, 
-		ReadTimeout: 10 * time.Second, 
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
@@ -54,6 +53,6 @@ func main() {
 
 	err := srv.ListenAndServe()
 	if err != nil {
-	log.Println(err)
+		log.Println(err)
 	}
 }
