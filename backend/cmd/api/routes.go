fcefd15b305b251612cad40af5e57064b46a3726
george <gigwegbe@gmail.com>
2021-08-04 06:35:01 +0100

    Adding error handler

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -1,15 +1,17 @@
-package main 
+package main
 
 import (
 	"net/http"
+
 	"github.com/julienschmidt/httprouter"
 )
 
-
-func(app *application) routes() *httprouter.Router{
+func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
-	
-	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
-	return router 
-}
 
+	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
+
+	return router
+}
--- backend/models/models.go
+++ backend/models/models.go
@@ -14,7 +14,7 @@ type Movie struct {
 	Rating      int          `json:"rating"`
 	MPAARating  string       `json:"mpaa_rating"`
 	CreatedAt   time.Time    `json:"created_at"`
-	UpdatedAt   time.Time    `json:"updatedAt"`
+	UpdatedAt   time.Time    `json:"updated_at"`
 	MovieGenre  []MovieGenre `json:"_"`
 }
 
@@ -22,7 +22,7 @@ type Genre struct {
 	ID        int       `json:"id"`
 	GenreName string    `json:"genre_name"`
 	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type MovieGenre struct {
@@ -31,5 +31,5 @@ type MovieGenre struct {
 	GenreID   int       `json:"genre_id"`
 	Genre     Genre     `json:"genre"`
 	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
